2458d423ea46e1da082073f0f5da8d7bb61f5d43
michelleGomes85 <gmichele498@gmail.com>
2025-02-25 18:38:53 -0300

    improvent padding

--- src/styles.js
+++ src/styles.js
@@ -31,7 +31,7 @@ export const Content = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
-    padding: 10px;
+    padding: 20px 10px;
 `
 
 export const Column = styled.div`
